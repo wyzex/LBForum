2f6f254d23df359e2544dd1bd19bf31ed043d4b8
wyzex <ryzex@yandex.ru>
2014-05-22 01:03:05 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -78,9 +78,6 @@ setup(
     description = 'LBForum is a quick and simple forum which uses the Django Framework.',
     packages = packages,
     cmdclass = cmdclasses,
-    dependency_links = [
-    'https://github.com/slepa/django-simple-avatar.git#egg=django-simple-avatar-fork',
-    ],
     install_requires=[
         "BeautifulSoup==3.2.1",
         "Django==1.4",
@@ -95,7 +92,6 @@ setup(
         "django-simple-captcha==0.4.2",
         "postmarkup==1.2.0",
         "six==1.4.1",
-        "django-simple-avatar-fork",
         "wsgiref==0.1.2",
         "pytils==0.2.3",
         ],
